512db3dc7e895e23a059efb854a8d4b64699aee8
heinvde <heinrich.vdende@gmail.com>
2025-04-03 08:38:42 +0200

    Allow environment variable to be set in project config

--- app/project.clj
+++ app/project.clj
@@ -4,12 +4,18 @@
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [org.clojure/data.json "2.5.1"]]
-  :plugins [[lein-ring "0.12.5"]]
+                 [org.clojure/data.json "2.5.1"]
+                 [environ "1.2.0"]]
+  :plugins [[lein-ring "0.12.5"]
+            [lein-environ "1.2.0"]]
   :repl-options {:init-ns yuppie-assessment.app}
   :ring {:handler yuppie-assessment.app/app
          :port 8080
          :auto-reload? true}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]]
+         :env {:google-client-id "fake-google-client-id"
+               :google-client-secret "fake-google-client-secret"
+               :google-oauth2-redirect-uri "http://localhost:8080/auth/verified"
+               :google-oauth2-state-key "fake-state-key"}}})
