af8495853356c1107c493b9391603c53b49cea6b
heinvde <heinrich.vdende@gmail.com>
2025-04-04 13:27:47 +0200

    Remove 'profile_picture_url' column from 'user_profiles' table

--- databases/users/001_create_tables.sql
+++ databases/users/001_create_tables.sql
@@ -3,8 +3,7 @@ CREATE TABLE user_profiles (
     id VARCHAR(40) PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100),
-    email VARCHAR(320) NOT NULL UNIQUE,
-    profile_picture_url TEXT
+    email VARCHAR(320) NOT NULL UNIQUE
 );
 
 CREATE TABLE user_profile_pictures (
