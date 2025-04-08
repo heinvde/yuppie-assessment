b45db7e502705f7b5a889e7782efe40af39465db
heinvde <heinrich.vdende@gmail.com>
2025-04-08 09:23:29 +0200

    Handle known cloudinary exceptions

--- app/src/yuppie_assessment/cloudinary/client.clj
+++ app/src/yuppie_assessment/cloudinary/client.clj
@@ -1,5 +1,25 @@
 (ns yuppie-assessment.cloudinary.client
+  (:require [clojure.string :as clj-str]
+            [yuppie-assessment.logger :refer [log-error]])
   (:import (com.cloudinary Cloudinary)))
+
+(def error-unauthorized :error-unauthorized)
+(def error-resource-not-found :error-resource-not-found)
+(def error-cloudname-not-found :error-cloudname-not-found)
+
+(defn- handle-exception [ex]
+  (let [message (.getMessage ex)]
+    (log-error "Cloudinary error:" message)
+    (cond
+      (clj-str/includes? message "Resource not found")
+      (throw (ex-info message {:type error-resource-not-found}))
+      (clj-str/includes? message "Invalid api_key")
+      (throw (ex-info message {:type error-unauthorized}))
+      (clj-str/includes? message "Invalid cloud_name")
+      (throw (ex-info message {:type error-cloudname-not-found}))
+      (clj-str/includes? message "Invalid Signature")
+      (throw (ex-info message {:type error-unauthorized}))
+      :else (throw ex)))) ;unkown error
 
 (defn- format-upload-response [response]
   {:url (get response "secure_url")
@@ -10,10 +30,13 @@
 (defn upload-image-from-url
   ([client-spec url] (upload-image-from-url client-spec url {}))
   ([client-spec url settings]
-   (some-> client-spec
-           :url
-           (Cloudinary.)
-           (.uploader)
-           (.upload url (merge {"resource_type" "image" "overwrite" true "use_name" true}
-                               settings))
-           (format-upload-response))))
+   (try
+     (some-> client-spec
+             :url
+             (Cloudinary.)
+             (.uploader)
+             (.upload url (merge {"resource_type" "image" "overwrite" true "use_name" true "timeout" 1}
+                                 settings))
+             (format-upload-response))
+     (catch Exception e
+       (handle-exception e)))))
